c7245db63223352a443d79eeb849f7c9c5935c96
Carlos Portella <carlosportella16@hotmail.com>
2022-10-19 20:08:46 -0300

    feature/atividade-07
    Criando projeto com angular-CLI e dois componentes

---
+++ divide-ai/src/app/tela-about/tela-about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-about',
+  templateUrl: './tela-about.component.html',
+  styleUrls: ['./tela-about.component.css']
+})
+export class TelaAboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ divide-ai/src/app/tela-inicio/tela-inicio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-inicio',
+  templateUrl: './tela-inicio.component.html',
+  styleUrls: ['./tela-inicio.component.css']
+})
+export class TelaInicioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
